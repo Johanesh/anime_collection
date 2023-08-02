fc3255ad0d1eddd3970b25353bd197d9113deac3
Johanes <johanesh11@gmail.com>
2023-08-02 16:15:43 +0700

    paginate arrow add icon start and end

--- src/components/features/Paginate/index.tsx
+++ src/components/features/Paginate/index.tsx
@@ -5,13 +5,16 @@ import Box from "@/components/user-interfaces/Box";
 import FixedBox from "@/components/user-interfaces/FixedBox";
 import Grid from "@/components/user-interfaces/Grid";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faChevronLeft, faChevronRight } from "@fortawesome/free-solid-svg-icons";
+import { faChevronLeft, faBackwardStep, faChevronRight, faForwardStep } from "@fortawesome/free-solid-svg-icons";
 
 export default function Paginate() {
     return (
         <FixedBox width="100%" maxWidth="768px" bottom="0" zIndex={9}>
             <Box color="#fff" padding="8px 16px" backgroundColor="#000" zIndex={2}>
-                <Grid textAlign="center" templateCol="20px 30px 15px 30px 20px" justify="center">
+                <Grid textAlign="center" templateCol="20px 20px 30px 15px 30px 20px 20px" justify="center" gap="8px">
+                    <Box cursor="pointer">
+                        <FontAwesomeIcon icon={faBackwardStep} />
+                    </Box>
                     <Box cursor="pointer">
                         <FontAwesomeIcon icon={faChevronLeft} />
                     </Box>
@@ -26,6 +29,9 @@ export default function Paginate() {
                     </Box>
                     <Box cursor="pointer">
                         <FontAwesomeIcon icon={faChevronRight} />
+                    </Box>
+                    <Box cursor="pointer">
+                        <FontAwesomeIcon icon={faForwardStep} />
                     </Box>
                 </Grid>
             </Box>
